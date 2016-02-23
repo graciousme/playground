f984f9d9d3ec310b7ad20fda5fe2939ee46e787e
Grace Yau <grace.yau@gmail.com>
2016-02-22 23:07:50 -0800

    midsessions_io comments

--- midsessions_io.py
+++ midsessions_io.py
@@ -1,10 +1,3 @@
-# Write a program that counts how many times each letter appears in a file. 
-# Return a dictionary. Test your program with this file.
-# Write a program that counts how many times each word appears in a file. 
-# Return a dictionary. Download this file to test your program. 
-# (Hints: Be careful of uppercase and lowercase instances of the same word. 
-# Remove punctuation at the end of words.)
-
 # returns dictionary with count of each character in a file, including punctuation and excluding spaces and line breaks
 def letter_count(file_name):
 	letter_count_dict = {}
@@ -17,12 +10,20 @@ def letter_count(file_name):
 				index  += 1
 	print letter_count_dict
 
-#letter_count("one_fish.txt")
+letter_count("one_fish.txt")
 
-# work in progress: working on returning count of each word in a file
+# returns dictionary with count of each word in a file
+# bug is that it returns count of ''
+# is there a more efficient way vs. replacing every single special character?
+
+# reads the file as a string
+# replaces special characters with spaces
+# then splits the string at spaces
+
 def word_count(file_name):
 	word_count_dict = {}
 	index = 0
+
 	with open (file_name) as word_count_file:
 		string = word_count_file.read()
 		string = string.replace("\n", " ")
@@ -32,8 +33,8 @@ def word_count(file_name):
 		string = string.replace("\x92t", "'")
 		string = string.replace("  ", " ")	
 		string = string.split(" ")
-	
-		# clean_file = ((" ".join(word_count_file).strip()).split(" ")
+
+# iterates over every word in the string except for spaces	
 		for word in string:
 			if word != " ":
 				word_count_dict[str.lower(word)] = string.count(word)
