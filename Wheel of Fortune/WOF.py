b358e4141cc3bdeede7abdec14b83ae2b679beda
Grace Yau <grace.yau@gmail.com>
2016-03-01 13:25:23 -0800

    fixed some buy_vowel issues

--- Wheel of Fortune/WOF.py
+++ Wheel of Fortune/WOF.py
@@ -62,49 +62,56 @@ def get_board_solution():
 	return board_solution
 
 # gets user's action for this turn, returns as user_choice
-def get_user_choice():
+def get_user_choice(user_score):
 	user_choice = raw_input ("\n\t1 = Spin the wheel and guess a letter \n\t2 = Buy a vowel for $250 \n\t3 = Solve the puzzle \n\n\tChoose an action: ")
 
-	if user_choice != "1" and user_choice != "2" and user_choice != "3":
-		user_choice = raw_input("\n\tSorry, that's not an option. Try again: ")
+	while user_choice == "2" and user_score < 250:
+		user_choice = raw_input("\n\tSorry, you need at least $250 to buy a vowel. Try again: ")
+
+	while user_choice not in ["1", "2", "3"]:
+		user_choice = raw_input("\n\tSorry, that's not an option. Try again: ")	
 
 	return user_choice
 
 # gets the space that user lands on, returns as landed_space
-# if lands on $1MM, give them 1 in 3 chance of getting it; otherwise Bankrupt
 # TO DO: code for non-$ wedges
 def spin_wheel():
 	with open("wheel_spaces.txt") as file:
 		lines = file.read().splitlines()
-		
 		landed_space = random.choice(lines)
 
-		if landed_space == "ONE MILLION":
-			rand_int = random.randint(0, 2)
-			if rand_int != 1:
-				landed_space = random.choice(lines)
-			else:
-				landed_space = "$1000000"
-				print "\n\t YOU LANDED ON THE $1,000,000 SPACE, CONGRATS! "
+	return landed_space
 
-	print "\n\tWheel has landed on:", landed_space
+# if lands on $1MM, give them 1 in 3 chance of getting it; otherwise Bankrupt
+# TO DO: make sure bankrupt works
+def landed_on_million():
+	rand_int = random.randint(0, 2)
+	
+	if rand_int != 1:
+		landed_space = "BANKRUPT"
+
+	else:
+		landed_space = "$1000000"
+		print "\n\t YOU LANDED ON THE $1,000,000 SPACE, CONGRATS! "
+
 	return landed_space
 			
 # gets consonant that user chooses, returns as guess
 # TO DO: handle other non-letters, previously guessed letters
 def guess_letter():
 	guess = str.upper(raw_input ("\n\tGuess a letter as the wheel spins: "))
-	if len(guess) > 1:
-		print "\n\tSorry, just one letter please: "
-		guess = str.upper(raw_input ("\n\tGuess a letter as the wheel spins: "))
+	
+	while len(guess) > 1:
+		guess = str.upper(raw_input ("\n\tSorry, just one letter please: "))
 	
 	return guess
 
 # gets vowel that user chooses, returns as guess
 # TO DO: handle invalid inputs, users with < $250
-def buy_vowel(user_score):
+def buy_vowel():
 	guess = str.upper(raw_input ("\n\tWhich vowel would you like to buy? "))
-	if guess not in ['A', 'E', 'I', 'O', 'U']:
+	
+	while guess not in ['A', 'E', 'I', 'O', 'U']:
 		guess = str.upper(raw_input ("\n\tSorry, that's an invalid entry. Please try again: "))
 	
 	return guess
@@ -114,8 +121,10 @@ def buy_vowel(user_score):
 def solve_puzzle(board_solution):
 	puzzle_guess = str.upper(raw_input("\n\tImpressive! Please type your guess: "))
 	answer = ''.join(board_solution)
+	
 	if puzzle_guess == answer:
 		return True
+	
 	else:
 		return False
 
@@ -137,41 +146,56 @@ def draw_board(guesses, board_solution, category):
 		else:
 			print "__",
 
-# checks if user's guess was correct, returns as is_winning boolean
-def check_if_winning(guess, board_solution):
+# gets score for this turn only
+# prints message based on whether user earned points
+# handles dollar amounts as integers
+# TO DO: handle other wedges like Bankrupt, Lose a Turn, Prize, 1/2 Car
+def calc_score_wheel(guess, board_solution, landed_space):	
 	count_of_letter = board_solution.count(guess)
+	
+	if count_of_letter > 0:
 
+		landed_space = int(landed_space.replace("$", ""))
+		turn_score = landed_space * count_of_letter
+		
+	else:
+		turn_score = 0
+
+	return turn_score
+
+# if user lands on bankrupt, drain user score to 0, return new user_score?? does this make sense as a function?
+def landed_on_bankrupt(user_score):
+	user_score = 0
+
+	print "\n\tSo sorry, you've landed on BANKRUPT! Your score is %i, and it's the next contestant's turn." % (user_score)
+
+	return user_score
+
+# prints message based on whether letter is in board, returns is_winning = True if letter was correct
+# TO DO: message is awkward for vowels ("-250 earned")
+def check_letter(guess, board_solution, turn_score):
+	count_of_letter = board_solution.count(guess)
+	
 	if count_of_letter > 0:
 		is_winning = True
+
+		if count_of_letter == 1:
+			print "\n\t>>> There is %i %s on the board! You've earned %i this round." % (count_of_letter, guess, turn_score)
+			
+		else:
+			print "\n\t>>> There are %i %ss on the board! You've earned %i in this round." % (count_of_letter, guess, turn_score)
+		
 	else:
+		print "\n\t>>> There are no %ss on the board, sorry! Next contestant." % (guess)
 		is_winning = False
 
 	return is_winning
 
-# gets score for this turn only
-# handles dollar amounts as integers
-# TO DO: handle other wedges like Bankrupt, Lose a Turn, Prize, 1/2 Car
-def get_turn_score(guess, board_solution, landed_space):	
-	count_of_letter = board_solution.count(guess)
-	
-	if count_of_letter > 0:
-		if not landed_space.startswith("$"):
-			turn_score = 0
-
-		else:
-			landed_space = int(landed_space.replace("$", ""))
-			turn_score = landed_space * count_of_letter
-			if count_of_letter == 1:
-				print "\n\tThere is %i %s on the board! You've earned %i this round." % (count_of_letter, guess, turn_score)
-			else:
-				print "\n\tThere are %i %ss on the board! You've earned %i in this round." % (count_of_letter, guess, turn_score)
-
-	else:
-		turn_score = 0
-		print "\n\tThere are no %ss on the board, sorry! Next contestant." % (guess)
-
-	return turn_score
-
+def reveal_board(category, board_solution):
+	print "\n\tCategory: %s\n\n" % (category) 
+	print "\t",
+	for letter in board_solution:
+		print letter,
 
 # plays game while condition is True
 # increments score for users
@@ -184,17 +208,20 @@ def play_game():
 
 	users = get_user_names(num_users)
 
-	guesses = []
+	scores = {}
+	for user in users:
+		scores[user] = 0
 
 	category = get_category()
 
 	board_solution = get_board_solution()
 
-	draw_board(guesses, board_solution, category)
+## for testing
+	print board_solution
 
-	scores = {}
-	for user in users:
-		scores[user] = 0
+	guesses = []
+
+	draw_board(guesses, board_solution, category)
 
 	user_index = 0
 
@@ -202,51 +229,78 @@ def play_game():
 
 		current_user = users[user_index]
 
-	 	user_score = scores[current_user]
-
 		print "\n\n\tCurrent score is ", scores
 		user_score = scores[current_user]
 		
 		raw_input ("\n\tPress 'enter' to start turn ")
 
-		print "\n\n %s's turn" % (current_user)
+		print "\n\n > %s's turn" % (current_user)
 		
-		user_choice = get_user_choice()
+		user_choice = get_user_choice(user_score)
 		
 		if user_choice == "1":
 			guess = guess_letter()
 			
 			landed_space = spin_wheel()
+
+			if landed_space == "ONE MILLION":
+				
+				landed_space = landed_on_million()
+				## pass through variabe for bankrupt situation?
+
+			#if landed_space == "BANKRUPT":
+				## TO DO: code this
+
+			print "\n\t>> Wheel has landed on: %s" % (landed_space)
 			
 			raw_input ("\n\tPress 'enter' to see results ")
 
 			guesses = store_guesses(guess, guesses)
 
-			turn_score = get_turn_score(guess, board_solution, landed_space)
+			turn_score = calc_score_wheel(guess, board_solution, landed_space)
+
+			is_winning = check_letter(guess, board_solution, turn_score)	
 
 			draw_board(guesses, board_solution, category)
 
 			scores[current_user] += turn_score
-
-			is_winning = check_if_winning(guess, board_solution)
 
 			if is_winning == False:
 				user_index += 1
 				user_index %= num_users	
 
 		if user_choice == "2":
-			guess = buy_vowel(user_score)
+			guess = buy_vowel()
 
 			guesses = store_guesses(guess, guesses)
 
-			user_score -= 250
+			turn_score = -250
+
+			is_winning = check_letter(guess, board_solution, turn_score)
 
 			draw_board(guesses, board_solution, category)
+
+			scores[current_user] += turn_score
+
+			if is_winning == False:
+				user_index += 1
+				user_index %= num_users	
 
 		if user_choice == "3":
 			is_solved = solve_puzzle(board_solution)
 
+			if is_solved:
+				print "\n\t>>> Nice work! You solved ths puzzle."
 
+				reveal_board(category, board_solution)
+				# TO DO: add bonus amount
+				# TO DO: start next round
+			
+			else:
+				print "\n\t>>> Sorry, that's not right"
+				# take away from score
+				user_index += 1
+				user_index %= num_users	
 
 
 play_game()
